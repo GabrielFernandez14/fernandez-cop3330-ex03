d225ca039028382f742bd140b1ef30fd7be66ed0
Gabriel Desktop <gabrielfernandez@knights.ucf.edu>
2021-05-28 15:18:02 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+package org.example;
+import java.util.Scanner;
+
+public class App {
+
+    // Main function
+    public static void main(String[] args) {
+        // Prompt the user for input, and scan until the next line is reached
+        // Store the scanner input into the String datatype that is quote
+        System.out.println("What is the quote?");
+        Scanner quoteInput = new Scanner(System.in);
+        String quote = quoteInput.nextLine();
+
+        // Repeat for the author
+        System.out.println("Who said it?");
+        Scanner authorInput = new Scanner(System.in);
+        String author = authorInput.nextLine();
+
+        // Print out the exit statement, with "\"" surrounding the quote
+        System.out.println(author + " says, " + "\"" + quote + "\"");
+    }
+}
